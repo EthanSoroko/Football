55ffe0429433ca95866c4e2dfc08989e02741c66
EthanSoroko <sorokoe@bc.edu>
2025-01-15 12:03:33 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Ethan Soroko on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
